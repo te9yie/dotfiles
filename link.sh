27fc091a124eff680d9753a64b51b8516d2310bf
te9yie <te9yie@users.noreply.github.com>
2016-12-03 14:25:47 +0900

    update.

--- link.sh
+++ link.sh
@@ -3,6 +3,10 @@
 DOTDIR=$(cd $(dirname ${BASH_SOURCE:-${0}}); pwd)
 for i in $(ls ${DOTDIR} |grep -v 'link.sh')
 do
+	[ ! -f ${DOTDIR}/${i} ] && continue
 	ln -sf ${DOTDIR}/${i} ${HOME}/.${i}
 	echo ln -sf ${DOTDIR}/${i} ${HOME}/.${i}
 done
+mkdir -p ${HOME}/.vim
+ln -sf ${DOTDIR}/vim/* ${HOME}/.vim/
+echo ln -sf ${DOTDIR}/vim/* ${HOME}/.vim/
