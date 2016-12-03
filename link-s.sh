12d28f45897c9bf14f0cff69d31ced3c42119ccb
te9yie <te9yie@users.noreply.github.com>
2016-12-03 23:10:24 +0900

    update.

--- link-s.sh
+++ link-s.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DOTDIR=$(cd $(dirname ${BASH_SOURCE:-${0}}); pwd)
-for i in $(ls ${DOTDIR} |grep -v 'link.sh')
+for i in $(ls ${DOTDIR} |grep -v '\.sh')
 do
 	[ ! -f ${DOTDIR}/${i} ] && continue
 	ln -sf ${DOTDIR}/${i} ${HOME}/.${i}
